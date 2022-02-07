46cbe70770157411143e82e515a8184f4930fa38
titikkoma88 <jie.koma88@gmail.com>
2022-02-08 00:12:27 +0700

    categories delete

--- server-toko-buku/app/api/categories/controller.js
+++ server-toko-buku/app/api/categories/controller.js
@@ -56,4 +56,19 @@ module.exports = {
             next(error);
         }
     },
+
+    deleteCategories : (req, res, next)=>{
+        Category.findOne({ 
+            where: { id: req.params.id, user: req.user.id },
+        }).then((categories)=>{
+            if(categories){
+                categories.destroy();
+
+                res.status(200).json({
+                    message: 'Success delete categories',
+                    data: categories,
+                });
+            }
+        }).catch((error)=>next(error));
+    }
 };
--- server-toko-buku/app/api/categories/router.js
+++ server-toko-buku/app/api/categories/router.js
@@ -6,5 +6,6 @@ const controller = require('./controller');
 router.get('/categories', auth, controller.getAllCategories);
 router.post('/categories', auth, controller.createCategories);
 router.put('/categories/:id', auth, controller.updateCategories);
+router.delete('/categories/:id', auth, controller.deleteCategories);
 
 module.exports = router;
