209fd4ea4e5a7bb8ca90a85c0fe93e35d1b098ac
titikkoma88 <jie.koma88@gmail.com>
2022-02-09 06:27:27 +0700

    books read

---
+++ server-toko-buku/app/api/books/router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../../middlewares/auth');
+const controller = require('./controller');
+
+router.get('/books', auth, controller.getAllBooks);
+// router.post('/categories', auth, controller.createCategories);
+// router.put('/categories/:id', auth, controller.updateCategories);
+// router.delete('/categories/:id', auth, controller.deleteCategories);
+
+module.exports = router;
